f8e151e4919e8c35c2eb5e0f73d6781511887b9b
Mr-Thop <karthikreddy0314@gmail.com>
2025-02-01 17:40:15 +0530

    Creating Method sumArray

--- Calculator.java
+++ Calculator.java
@@ -29,4 +29,15 @@ public class Calculator {
             return fibonacci(n - 1) + fibonacci(n - 2);
         }   
     }
+
+    public double sumArray(double[] arr) {
+        double sum = 0;
+
+        for (double num : arr) {
+            sum += num;
+        }
+
+        return sum;
+    }
+    
 }
