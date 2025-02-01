53d4083f2d840e829a37826cff7eec9ac11a8bf1
Mr-Thop <karthikreddy0314@gmail.com>
2025-02-01 17:36:36 +0530

    Creating Method multiply

--- Calculator.java
+++ Calculator.java
@@ -3,7 +3,12 @@ public class Calculator {
     public double add(double a, double b) { 
         return a + b; 
     }
+
     public double subtract(double a, double b) { 
         return a - b; 
+    }
+
+    public double multiply(double a, double b) { 
+        return a * b; 
     }
 }
